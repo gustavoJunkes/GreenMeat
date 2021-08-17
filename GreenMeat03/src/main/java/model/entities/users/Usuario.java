7625800a597d84b36ccbf8d717189644fec0ab6a
gustavoJunkes <87446204+gustavoJunkes@users.noreply.github.com>
2021-08-17 17:01:20 -0300

    Alterações na classe Usuario
    Comentarios hibernate e alterando para classe abstrata. Modelando as relações com outras classes

--- GreenMeat03/src/main/java/model/entities/users/Usuario.java
+++ GreenMeat03/src/main/java/model/entities/users/Usuario.java
@@ -1,26 +1,48 @@
 package model.entities.users;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToMany;
 
 import model.entities.users.information.Contato;
 import model.entities.users.information.Localidade;
 import model.entities.users.information.NivelAcesso;
 
-public class Usuario {
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-	private Localidade endereco;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	// um usuario possui diversas localidades, relação de N pra N
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	private List<Localidade> enderecos;
 	private String login; // nao vai login
 	private String senha;
 	private NivelAcesso nivelDeAcesso;
-	private Contato contato;     	// Contato aparece na classe Usuário e Funcionario          
+	private Contato contato; // Contato aparece na classe Usuário e Funcionario
 	private LocalDate dataCadastro;
 	private String sobrenome;
-	
+
 	// Cada vez que um usuário for criado seu tipo de acesso deve ser especificado
 	// de acordo com o usuario(cliente, funcionario, fornecedor).
 
 	public Usuario() {
-		
+
 	}
 
 	public Usuario(Localidade endereco, String login, String senha, Contato contato) {
@@ -82,6 +104,5 @@ public class Usuario {
 	public void setDataCadastro(LocalDate dataCadastro) {
 		this.dataCadastro = dataCadastro;
 	}
-	
-	
+
 }
