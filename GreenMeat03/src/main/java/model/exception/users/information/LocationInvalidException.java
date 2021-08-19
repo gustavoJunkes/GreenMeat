5558c9a15a1705dc9267839f4999e8c48bd5eaa1
MateusRaulino <masterdark235@gmail.com>
2021-08-18 21:49:08 -0300

    Exceções ajeitadas e exceção acrescentada no Estoque

--- GreenMeat03/src/main/java/model/exception/users/information/LocationInvalidException.java
+++ GreenMeat03/src/main/java/model/exception/users/information/LocationInvalidException.java
@@ -1,22 +1,10 @@
 package model.exception.users.information;
 
-import model.entities.users.information.Localidade;
-
 public class LocationInvalidException extends Exception {
-	
-	private Localidade localidade;
 
-	
-	
-	public LocationInvalidException(Localidade localidade) {
-		super();
-		this.localidade = localidade;
+	public LocationInvalidException(String mensagem) {
+		super(mensagem);
 
-	}
-	
-
-	public String toString() {
-		return "Localidade inválida";
 	}
 
 }
